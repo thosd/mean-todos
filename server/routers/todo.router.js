221c8efaf2fd0cee8da99abe419fabb5874408fc
Justin Dennison <justin1dennison@gmail.com>
2016-11-07 20:46:18 -0500

    implemented create route through the use of a post route

---
+++ server/routers/todo.router.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var Todo = require('../models/todo.model.js');
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+
+router.get('/todos', function(req, res){});
+router.get('/todos/:id', function(req, res){});
+router.post('/todos', function(req, res){
+  var todo = new Todo(req.body);
+  todo.save(function(err){
+    if(err){
+      // throw err; //don't do this! for now...
+      res.status(500).json({
+        err: err
+      });
+    }
+    res.status(201).json({
+      msg: 'successfully created todo'
+    });
+  });
+});
+router.put('/todos/:id', function(req, res){});
+router.delete('/todos/:id', function(req, res){});
+
+module.exports = router;
