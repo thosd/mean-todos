15f54ef734d851d4a1d88762901968f64ec7729c
Justin Dennison <justin1dennison@gmail.com>
2016-11-08 19:40:54 -0500

    implemented the update route for a single todo

--- server/routers/todo.router.js
+++ server/routers/todo.router.js
@@ -44,7 +44,18 @@ router.post('/todos', function(req, res){
     });
   });
 });
-router.put('/todos/:id', function(req, res){});
+router.put('/todos/:id', function(req, res){
+  Todo.findOneAndUpdate({_id: req.params.id}, req.body, function(err, oldTodo){
+    if(err){
+      res.status(500).json({
+        err: err
+      });
+    }
+    res.status(200).json({
+      msg: oldTodo
+    });
+  });
+});
 router.delete('/todos/:id', function(req, res){
   Todo.findOneAndRemove({ _id: req.params.id }, function(err, deletedTodo){
     if(err){
